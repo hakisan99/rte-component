2fa60ee6f9eb2215b01c42224475140759c721f4
Hexagon <hexagon.study@gmail.com>
2021-03-31 15:45:40 +0700

    update icon for toolbar

--- src/components/StyledComponents.jsx
+++ src/components/StyledComponents.jsx
@@ -14,6 +14,7 @@ export const StyledButton = styled.button`
   background: ${props => props.active ? props.theme.color.background.secondary : props.theme.color.background.primary};
   border-bottom: ${props => props.active ? "1px solid " + props.theme.color.fill.primary : "none"};
   height: 1.5rem;
+  cursor: pointer;
 `
 export const StyledBody = styled.div`
   padding: 0.4rem;
@@ -26,6 +27,11 @@ export const StyledTable = styled.table`
     border: '1px red solid'; 
     border-collapse: 'collapse'; 
     width:"100%";
+`
+export const VerticalLine = styled.div`
+  min-height: 100%;
+  width: 1px;
+  background: ${props => props.theme.color.border.primary};
 `
 import {H1, H2, H3, H4, H5, H6, P, HL} from './Typography'
 
