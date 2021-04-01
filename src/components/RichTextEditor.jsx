ba5ede17b2bedc684f48eb60aa560f9925547095
hakisan99 <sonngo81.99@gmail.com>
2021-04-01 17:31:58 +0700

    feat(table-options): implement basic table options with right click, the positioning code suck atm

--- src/components/RichTextEditor.jsx
+++ src/components/RichTextEditor.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useMemo, useState, useCallback } from 'react';
+import React, { useMemo, useState, useCallback, useRef } from 'react';
 import PropTypes from "prop-types";
 import { createEditor } from 'slate';
 import { Slate, Editable, withReact } from 'slate-react';
@@ -14,6 +14,9 @@ import {
 } from './StyledComponents';
 import { AlignButton, BlockButton, IndenButton, MarkButton, AddTableButton, TableButton, FontSizeButton, TextColor, TextHighlight } from './ToolbarButtons';
 import Toggle from './Toggle';
+import TableOptions from './popupComponents/TableOptions';
+import { tableCheck } from './editor-tools/tableUtil';
+import useClickOutside from '../hooks/useClickOutside';
 
 
 const RichTextEditor = (props) => {
@@ -34,47 +37,89 @@ const RichTextEditor = (props) => {
   const renderLeaf = useCallback((props) => {
     return <Leaf {...props} />;
   }, []);
+
+  //Table Options
+  const [openTableOptions, setOpenTableOptions] = useState(false);
+  const [tableOptionsPosition, setTableOptionsPosition] = useState({top: 0, left: 0});
+  const ref = useClickOutside(() => setOpenTableOptions(false));
+  const handleOpenTableOptions = (e) => {
+    const isTable = tableCheck(editor);
+    if (isTable) {
+      // Not sure how to implement this, so here's some hard code
+      setTableOptionsPosition({top: e.nativeEvent.pageY + 24, left: e.nativeEvent.pageX + 48})
+      setOpenTableOptions(true);
+    }
+  }
   return (
-    <StyledContainer>
+    <StyledContainer ref={ref}>
       <Slate
         editor={editor}
         value={value}
         onChange={(newVal) => setValue(newVal)}
       >
         <StyledToolbar>
-            <MarkButton  format="bold" text="Bold" icon="bold"/>
-            <MarkButton  format="italic" text="Italic" icon="italic"/>
-            <MarkButton  format="underline" text="Underline" icon="underline"/>
-            <VerticalLine />
-            <FontSizeButton text='Font Size' />
-            <TextColor text='Text Color' />
-            <TextHighlight text='Highlight text' />
-            <VerticalLine/>
-            <BlockButton format="h1" text="Heading 1" icon="heading 1"/>
-            <BlockButton format="h2" text="Heading 2" icon="heading 2"/>
-            <BlockButton format="h3" text="Heading 3" icon="heading 3"/>
-            <VerticalLine/>
-            <IndenButton format="increase" text="Increase Indentation" icon="increase-indentation"/>
-            <IndenButton format="decrease" text="Decrease Indentation" icon="decrease-indentation"/>
-            <BlockButton format="ol" text="Ordered List" icon="ordered-list"/>
-            <BlockButton format="ul" text="Bullet List" icon="unordered-list"/>
-            <VerticalLine/>
-            <AlignButton format="left" text="Align Left" icon="align-left"/>
-            <AlignButton format="center" text="Align Center" icon="align-center"/>
-            <AlignButton format="right" text="Align Right" icon="align-right"/>
-            <AlignButton format="justify" text="Align Justify" icon="align-justify"/>
-            <VerticalLine/>
-            <AddTableButton text="Add Table" icon="table"/>
-            <TableButton format="insert-row" text="Insert Row"/>
-            <TableButton format="insert-column" text="Insert Column"/>
-            <TableButton format="remove-row" text="Remove Row"/>
-            <TableButton format="remove-column" text="Remove Column"/>
-            <VerticalLine/>
-            <Toggle value={props.isDark} onSelect={() => props.setIsDark(!props.isDark)}/>
+          <MarkButton format="bold" text="Bold" icon="bold" />
+          <MarkButton format="italic" text="Italic" icon="italic" />
+          <MarkButton format="underline" text="Underline" icon="underline" />
+          <VerticalLine />
+          <FontSizeButton text="Font Size" />
+          <TextColor text="Text Color" />
+          <TextHighlight text="Highlight text" />
+          <VerticalLine />
+          <BlockButton format="h1" text="Heading 1" icon="heading 1" />
+          <BlockButton format="h2" text="Heading 2" icon="heading 2" />
+          <BlockButton format="h3" text="Heading 3" icon="heading 3" />
+          <VerticalLine />
+          <IndenButton
+            format="increase"
+            text="Increase Indentation"
+            icon="increase-indentation"
+          />
+          <IndenButton
+            format="decrease"
+            text="Decrease Indentation"
+            icon="decrease-indentation"
+          />
+          <BlockButton format="ol" text="Ordered List" icon="ordered-list" />
+          <BlockButton format="ul" text="Bullet List" icon="unordered-list" />
+          <VerticalLine />
+          <AlignButton format="left" text="Align Left" icon="align-left" />
+          <AlignButton
+            format="center"
+            text="Align Center"
+            icon="align-center"
+          />
+          <AlignButton format="right" text="Align Right" icon="align-right" />
+          <AlignButton
+            format="justify"
+            text="Align Justify"
+            icon="align-justify"
+          />
+          <VerticalLine />
+          <AddTableButton text="Add Table" icon="table" />
+          <TableButton format="insert-row" text="Insert Row" />
+          <TableButton format="insert-column" text="Insert Column" />
+          <TableButton format="remove-row" text="Remove Row" />
+          <TableButton format="remove-column" text="Remove Column" />
+          <VerticalLine />
+          <Toggle
+            value={props.isDark}
+            onSelect={() => props.setIsDark(!props.isDark)}
+          />
         </StyledToolbar>
         <StyledBody>
-          <Editable renderElement={renderElement} renderLeaf={renderLeaf} autoFocus spellCheck={false}/>
+          <Editable
+            renderElement={renderElement}
+            renderLeaf={renderLeaf}
+            autoFocus
+            spellCheck={false}
+            onContextMenu={(e) => {
+              e.preventDefault();
+              handleOpenTableOptions(e);
+            }}
+          />
         </StyledBody>
+        {openTableOptions && <TableOptions position={tableOptionsPosition} setOpenTableOptions={setOpenTableOptions} />}
       </Slate>
     </StyledContainer>
   );
---
+++ src/components/popupComponents/TableOptions.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { useSlate } from 'slate-react';
+import {insertRow, insertColumn, removeRow, removeColumn} from '../editor-tools/tableUtil';
+
+
+const TableOptionsWrapper = styled.ul`
+  position: absolute;
+  top: 120%;
+  left: 50%;
+  transform: translateX(-50%);
+  background: ${(props) => props.theme.color.background.secondary};
+  border: 1px solid ${(props) => props.theme.color.border.primary};
+  border-radius: 4px;
+  box-shadow: ${(props) => props.theme.shadow};
+  box-sizing: border-box;
+  list-style-type: none;
+`
+
+const TableOption = styled.li`
+  color: ${(props) => props.theme.color.text.primary};
+  padding: 0.4rem 0.6rem;
+  cursor: pointer;
+  &:hover {
+    color: ${props => props.theme.color.fill.info};
+    background: ${props => props.theme.color.background.secondary};
+  }
+`
+
+const TableOptions = ({position, setOpenTableOptions}) => {
+  const editor = useSlate();
+  return (
+    <TableOptionsWrapper style={{top: position.top + 'px', left: position.left + 'px'}}>
+      <TableOption onMouseDown={() => {insertRow(editor); setOpenTableOptions(false)}}>Insert Row</TableOption>
+      <TableOption onMouseDown={() => {insertColumn(editor); setOpenTableOptions(false)}}>Insert Column</TableOption>
+      <TableOption onMouseDown={() => {removeRow(editor); setOpenTableOptions(false)}}>Remove Row</TableOption>
+      <TableOption onMouseDown={() => {removeColumn(editor); setOpenTableOptions(false)}}>Remove Column</TableOption>
+    </TableOptionsWrapper>
+  )
+};
+
+TableOptions.propTypes = {
+  position: PropTypes.shape({ top: PropTypes.number, left: PropTypes.number}),
+  setOpenTableOptions: PropTypes.func
+};
+
+export default TableOptions;
